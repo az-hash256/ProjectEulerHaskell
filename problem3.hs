acaf732b34b6d22de3704b6cd2f9fb696562dc6b
Azad Ellafi <arellafi@bu.edu>
2023-11-28 13:13:47 -0500

    Added problem descriptions to problems 1-3

--- problem3.hs
+++ problem3.hs
@@ -1,5 +1,9 @@
+--- Problem 3: The prime factors of 13195 are 5, 7, 13, and 29. What is the largest prime factor of the number 600851475143
+
+--- Number we want the prime factor of
 x =  600851475143
 
+--- Find the list of Prime Factors
 factors n i lst = 
   case n `mod` 2 of
    0 -> factors (n `div` 2) i (2 : lst)
@@ -7,7 +11,5 @@ factors n i lst =
             True  -> if (n `mod` i) == 0 then factors (n `div` i) i (i : lst) else factors n (i + 2) lst
             False -> if n > 2 then (n : lst) else lst
 
-
-listPrimeFactors n = factors n 3 []
-
+--- Solution
 largestPrimeFactor n = maximum (factors n 3 [])
